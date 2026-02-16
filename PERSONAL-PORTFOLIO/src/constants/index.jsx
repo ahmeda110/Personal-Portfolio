42331a67dd6496941ac4e9d436c298c5be0d3337
Ahmed Abbas <abbas@pashmotors.com>
2026-02-15 19:32:20 -0700

    about me section done

---
+++ PERSONAL-PORTFOLIO/src/constants/index.jsx
@@ -0,0 +1,25 @@
+export const navLinks = [
+  {
+    name: "About Me",
+    link: "#work",
+  },
+  {
+    name: "Experience",
+    link: "#experience",
+  },
+  {
+    name: "Testimonials",
+    link: "#testimonials",
+  },
+];
+
+export const words = [ 
+    { text: 'Ideas', imgPath: '/images/ideas.svg'},
+    { text: 'Concepts', imgPath: '/images/concepts.svg'},
+    { text: 'Designs', imgPath: '/images/designs.svg'},
+    { text: 'Code', imgPath: '/images/code.svg'},
+    { text: 'Ideas', imgPath: '/images/ideas.svg'},
+    { text: 'Concepts', imgPath: '/images/concepts.svg'},
+    { text: 'Designs', imgPath: '/images/designs.svg'},
+    { text: 'Code', imgPath: '/images/code.svg'}
+]
---
+++ PERSONAL-PORTFOLIO/src/sections/Hero.jsx
@@ -0,0 +1,53 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+import Button from "../components/Button";
+import { words } from "../constants";
+import HeroExperience from "../components/HeroModels/HeroExperience";
+
+function Hero() {
+  return (
+    <section id="hero" className='relatice overflow-hidden'>
+        <div className='absolute top-0 left-0 z-10'>
+            <img src="/images/bg.png" alt="background" />
+        </div>
+        <div className='hero-layout'>
+            {/* left side of hero */}
+            <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
+                <div className='flex flex-col gap-7'>
+                    <div className='hero-text'>
+                        <h1 className=" hero-text-main font-black text-purple-300">Hi, I'm <span className="text-purple-600">Ahmed!</span></h1>
+                        <h1 >Shaping
+                            <span className="slide">
+                                <span className="wrapper">
+                                    {words.map((word) => (
+                                        <span key={word.text} className="flex items-center md:gap-3 gap-1 pb-2">
+                                            <img src={word.imgPath} alt={word.text} className="xl:size-10 md:size-8 size-5 md:p-2 p-1 rounded-full bg-white"/>
+                                            <span className="">{word.text}</span>
+                                        </span>
+                                    ))}
+                                </span>
+                            </span>
+                        </h1>
+                        <h1>Into Real Results</h1>
+                    </div>
+                    <p className="text-gray-400 md:text-xl relative z-10 pointer-events-none">
+                        Passionate About Creating Impactful Change Through Software :)
+                    </p>
+                    <Button 
+                        className="md:w-80 md:h-16 w-60 h-12" id="button" text="See my Work"/>
+                </div>
+            </header>
+
+            {/* right side of hero */}
+            <figure>
+                <div className="hero-3d-layout">
+                    <HeroExperience />
+                </div>
+            </figure>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
